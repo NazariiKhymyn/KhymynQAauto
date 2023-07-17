7009b0d29f120d7e3139c80213eba158ff9ad47a
NazariiKhymyn <mizuna@ukr.net>
2023-07-17 09:48:04 +0300

    fixed a bug with the second test

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -11,4 +11,4 @@ def test_first_request():
 @pytest.mark.http
 def test_second_request():
     r = requests.get("https://api.github.com/users/defunkt")
-    print(f"Response is {r.text}")
+    print(f"Response is {r.text.encode('utf-8')}")
