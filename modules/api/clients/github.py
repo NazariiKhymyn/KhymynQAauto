f43bb000284272f3dadf5a825e5176f7facc4dc5
NazariiKhymyn <mizuna@ukr.net>
2023-07-31 19:36:13 +0300

    Module 27. API testing. Project task 4 completed

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -9,7 +9,21 @@ class GitHub:
         return body
 
     def search_repo(self, name):
-        r = requests.get("https://api.github.com/search/repositories", params={"q": name})
+        r = requests.get(
+            "https://api.github.com/search/repositories", params={"q": name}
+        )
+        body = r.json()
+
+        return body
+
+    def search_emojis(self):
+        r = requests.get("https://api.github.com/emojis")
+        body = r.json()
+
+        return body
+
+    def get_commits(self, owner, repo):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
         body = r.json()
 
         return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -29,3 +29,57 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo("s")
     assert r["total_count"] != 0
+
+
+@pytest.mark.api
+def test_valid_object_url(github_api):
+    r = github_api.search_emojis()
+    assert (
+        r["+1"]
+        == "https://github.githubassets.com/images/icons/emoji/unicode/1f44d.png?v8"
+    )
+
+
+@pytest.mark.api
+def test_invalid_object_url(github_api):
+    r = github_api.search_emojis()
+    assert (
+        r["100"]
+        != "https://github.githubassets.com/images/icons/emoji/unicode/1f44d.png?v8"
+    )
+
+
+@pytest.mark.api
+def test_number_of_dictionary_items(github_api):
+    r = github_api.search_emojis()
+    assert len(r) == 1877
+
+
+@pytest.mark.api
+def test_key_is_present(github_api):
+    r = github_api.search_emojis()
+    assert "angel" in r
+
+
+@pytest.mark.api
+def test_checking_a_list_item(github_api):
+    r = github_api.get_commits("NazariiKhymyn", "KhymynQAauto")
+    assert r[0]["sha"] == "1fc0cfa4cded1d28706855c4de1e277dc2d99c8f"
+
+
+@pytest.mark.api
+def test_checking_mail_author_commit(github_api):
+    r = github_api.get_commits("NazariiKhymyn", "KhymynQAauto")
+    assert r[0].get("commit").get("author").get("email") == "mizuna@ukr.net"
+
+
+@pytest.mark.api
+def test_verification_of_login_committer(github_api):
+    r = github_api.get_commits("NazariiKhymyn", "KhymynQAauto")
+    assert r[0]["committer"]["login"] == "NazariiKhymyn"
+
+
+@pytest.mark.api
+def test_checking_message_commit(github_api):
+    r = github_api.get_commits("NazariiKhymyn", "KhymynQAauto")
+    assert r[20]["commit"]["message"] == "My first commit"
